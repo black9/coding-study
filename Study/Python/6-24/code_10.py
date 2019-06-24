60bc4c09ef859b96004008bfabeb29248678e8de
black9 <csu02049@naver.com>
2019-06-24 16:24:56 +0900

    Rename
    Rename

---
+++ Study/Python/6-24/code_10.py
@@ -0,0 +1,8 @@
+'''
+boolVar = True          ##불형
+intVar = 0              ##정수형 변수
+floatVar = 0.0          ##실수형 변수
+strVar = ""             ##문자형 변수
+
+'''
+type(boolVar), type(intVar), type(floatVar), type(strVar)
---
+++ Study/Python/6-24/code_11.py
@@ -0,0 +1,24 @@
+var1=var2=var3=var4=100
+
+print(var1, var2, var3, var4)
+
+
+
+var4 = 100
+var3 = var4
+var2 = var3
+var1 = var2
+
+print(var1, var2, var3, var4)
+
+
+
+myVar = 100
+type(myVar)
+
+myVar = 100.0
+type(myVar)
+
+myVar = 'Hello'
+type(myVar)
+
