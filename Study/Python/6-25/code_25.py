933ec3c14aa0cc50d5b8ddb026bae08bea4fcb13
black9 <csu02049@naver.com>
2019-06-25 17:00:24 +0900

    Today Study Finish
    bye bye

---
+++ Study/Python/6-25/code_25.py
@@ -0,0 +1,16 @@
+a = 200
+
+if a < 100:
+    print("100보다 작군요")
+    print("거짓이므로 이 문장은 안 보이겠죠?")
+
+print("프로그램 끝")
+
+
+score = int(input("정수를 입력해주세요:"))
+
+if score
+% 2 == 0 :
+    print("짝수입니다")
+else:
+    print("홀수입니다")
---
+++ Study/Python/6-25/code_26.1.py
@@ -0,0 +1,7 @@
+score = int(input("몇 점?"))
+res = ''
+if score >=60 :
+    res = '합격'
+else :
+    res = '불합격'
+print(res)
