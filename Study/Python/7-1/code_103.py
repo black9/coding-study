76fb00e3453de3b7c15dfaa56f4d98d3f7999b04
black9 <csu02049@naver.com>
2019-07-01 16:29:32 +0900

    Today Study Upload

---
+++ Study/Python/7-1/code_103.py
@@ -0,0 +1,19 @@
+from tkinter import *
+window = Tk()
+
+btnList = [None] * 3
+
+for i in range (0, 3):
+    btnList[i] = Button(window, text= "버튼" + str(i+1))
+
+for btn in btnList:
+
+    btn.pack(side=RIGHT)
+##    btn.pack(side=BOTTOM)
+##    btn.pack(side=TOP)
+##    btn.pack(side=TOP, fill = X)
+##    btn.pack(side=TOP, fill = X, padx = 10, pady = 10)
+##    btn.pack(side=TOP, fill = X, ipadx = 10, ipady = 10)
+##    btn.pack(side=Top, fill = X, ipadx = 10, ipady =10, padx = 10, pady = 10)    
+
+window.mainloop(0)
