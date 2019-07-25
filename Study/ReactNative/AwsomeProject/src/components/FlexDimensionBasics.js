ea4424bc0edb4a1cb5dfa27b89c48d6dafae9aff
black9 <csu02049@naver.com>
2019-07-25 17:27:23 +0900

    Today Study Upload
    - 문제
    Flex 방향 바꾸기

--- Study/ReactNative/AwsomeProject/src/components/FlexDimensionBasics.js
+++ Study/ReactNative/AwsomeProject/src/components/FlexDimensionBasics.js
@@ -4,7 +4,7 @@ import { View } from 'react-native';
 export default class FlexDimensionBasics extends Component{
     render() {
         return (
-            <View style={{flex: 1, width:'100%'}}>
+            <View style={{flex: 1, width:'100%', flexDirection:"row"}}>
                 <View style={{flex: 1, backgroundColor:'red'}}/>
                 <View style={{flex: 2, backgroundColor:'black'}}/>
                 <View style={{flex: 3, backgroundColor:'blue'}}/>     
