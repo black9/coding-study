8842742618871ef341dc3415d959cf3372736fab
black9 <csu02049@naver.com>
2019-07-25 18:00:31 +0900

    Today Study Upload Finish

---
+++ Study/ReactNative/AwsomeProject/src/components/Problem2.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { View, Text } from 'react-native';
+
+export default class Problem2 extends Component {
+    render(){
+        return (
+
+            <Text style={[fontSize:40 , fontWeight:'bold', this.props.style]}>
+                {this.props.childeren}
+            </Text>
+            )
+    }
+};
