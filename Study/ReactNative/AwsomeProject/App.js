94883ddebe1fad28429a2a74f4605773001e4e54
black9 <csu02049@naver.com>
2019-07-25 15:50:27 +0900

    Today Study Upload
    - 연습문제1
    네모 박스를 만들고 각 박스 Size 변경

--- Study/ReactNative/AwsomeProject/App.js
+++ Study/ReactNative/AwsomeProject/App.js
@@ -5,19 +5,20 @@ import Banana from './src/components/Banana';
 import GreetingGroup from './src/components/GreetingGroup';
 import BlinkApp from './src/components/BlinkApp';
 import StyleApp from './src/components/StyleApp';
-
+import Problem1 from './src/components/Problem1';
 
 export default function App() {
   return (
     <View style={styles.container}>
-        <Text>Hello React Native</Text>
+        {/*<Text>Hello React Native</Text>
         <Text>By Kim Hyeon Gu</Text>
         
         <Banana></Banana>
         <GreetingGroup></GreetingGroup>
         <BlinkApp></BlinkApp>
-       <StyleApp></StyleApp>
-       
+        <StyleApp></StyleApp> */}
+       <Problem1></Problem1>
+
     </View>
   );
 }
