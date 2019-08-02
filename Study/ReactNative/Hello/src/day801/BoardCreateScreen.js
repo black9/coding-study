f6e8f08c5a178333cfe7eb79699c03103a503df3
black9 <csu02049@naver.com>
2019-08-02 16:02:19 +0900

    Today Study Upload

--- Study/ReactNative/Hello/src/day801/BoardCreateScreen.js
+++ Study/ReactNative/Hello/src/day801/BoardCreateScreen.js
@@ -2,21 +2,33 @@ import React from 'react';
 import { View, FlatList, Text, TouchableOpacity } from 'react-native';
 import { removeOrientationChangeListener } from 'expo/build/ScreenOrientation/ScreenOrientation';
 import { TextInput } from 'react-native-gesture-handler';
+import { KeyboardAvoidingView } from 'react-native';
+
+
 
 import MyButton from './MyButton'
 
 export default class BoardCreateScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            content: '',
+        }
+    }
     render() {
         return (
             <View>
+
                 <Text style={{ fontSize: 30, color: 'blue', textAlign: 'center' }}> 글 쓰기 </Text>
 
-                <TextInput></TextInput>
-
-                <TextInput></TextInput>
-
-                <MyButton title="글 작성" />
-
+                <TextInput style={{ borderWidth: 1, fontSize: 20, height: 50, margin: 20, marginTop: 30 }} placeholder="제목" onChangeText={(text) => this.setState({ title: text })} />
+                <Text> {this.state.title}</Text>
+                <TextInput style={{ borderWidth: 1, minHeight: 300, margin: 20, marginTop: 50 }} placeholder="내용" multiline={true} onChangeText={(text) => this.setState({ content: text })} />
+                <Text> {this.state.content}</Text>
+                <View style={{ margin: 20, alignself: 'flex-end' }}>
+                    <MyButton title="글 작성" />
+                </View>
             </View>
 
         )
